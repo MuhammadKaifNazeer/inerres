fca80f4110f76c516e8efcebb2a6ba013426de06
Muhammad Kaif Nazeer <muhammadkaifnazeer99@gmail.com>
2024-04-14 18:26:46 +0500

    improved responsiveness

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -6,15 +6,15 @@ const Footer = () => {
       <div className="footer" id="footer">
         <div className="w-full flex flex-col bg-[--secondary-color] py-10 px-6">
           <div
-            className="font-bold text-5xl cursor-pointer flex justify-center items-center mb-10 
+            className="font-bold sm:text-5xl text-3xl cursor-pointer flex justify-center items-center mb-10 
       text-[--primary-text-color-black]"
           >
             <span className="h-[35px] w-[35px] rounded-full bg-[--primary-color] mr-2"></span>
             INERRES
           </div>
-        <div className="flex justify-between items-center bg-[--secondary-color] flex-wrap text-[--secondary-text-color]">
+        <div className="flex flex-col sm:flex-row justify-between items-center bg-[--secondary-color] flex-wrap text-[--secondary-text-color]">
           <p className="mx-1 hover:text-[--primary-text-color-black] duration-500 hover:underline cursor-pointer">Terms & Agreements</p>
-          <p className="mx-1 hover:text-[--primary-text-color-black] duration-500 hover:underline cursor-pointer">@2024 Inerres - Muhammad Kaif Nazeer. All Rights Reserved</p>
+          <p className="mx-1 hover:text-[--primary-text-color-black] duration-500 hover:underline cursor-pointer text-center">@2024 Inerres - Muhammad Kaif Nazeer. All Rights Reserved</p>
           <p className="mx-1 hover:text-[--primary-text-color-black] duration-500 hover:underline cursor-pointer">Privacy Policy</p>
         </div>
         </div>
